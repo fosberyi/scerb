126ed133392b2a9df51c27e1e7926ac694ec78d4
Ian Fosbery <ian@codeguy.io>
2016-05-06 16:27:28 -0400

    Moved sass functions to helper module

--- spec/integration/rendering_spec.rb
+++ spec/integration/rendering_spec.rb
@@ -17,31 +17,4 @@ describe ".scerb template handler" do
   def compress_string(str)
     str.gsub(/\s+/, '').gsub(/\n+/, '')
   end
-
-  def darken_color(hex, percentage)
-    base_sass_eval_context.darken(
-      base_sass_eval_context.hex_color(hex),
-      sass_number(percentage.to_i)
-    ).tap {|color|
-      color.options = base_sass_options
-    }.to_s
-  end
-
-  def base_sass_eval_context
-    @base_sass_eval_context ||= Sass::Script::Functions::EvaluationContext.new(base_sass_environment)
-  end
-
-  def base_sass_environment
-    @base_sass_environment ||= Sass::Environment.new(nil, base_sass_options)
-  end
-
-  def sass_number(int)
-    Sass::Script::Value::Number.new(int)
-  end
-
-  def base_sass_options
-    {
-       syntax: :scss,
-    }
-  end
 end
--- spec/support/sass_functions.rb
+++ spec/support/sass_functions.rb
@@ -1,1 +1,28 @@
+module SassFunctions
+  def darken_color(hex, percentage)
+    base_sass_eval_context.darken(
+      base_sass_eval_context.hex_color(hex),
+      sass_number(percentage.to_i)
+    ).tap {|color|
+      color.options = base_sass_options
+    }.to_s
+  end
 
+  def base_sass_eval_context
+    @base_sass_eval_context ||= Sass::Script::Functions::EvaluationContext.new(base_sass_environment)
+  end
+
+  def base_sass_environment
+    @base_sass_environment ||= Sass::Environment.new(nil, base_sass_options)
+  end
+
+  def sass_number(int)
+    Sass::Script::Value::Number.new(int)
+  end
+
+  def base_sass_options
+    {
+       syntax: :scss,
+    }
+  end
+end
